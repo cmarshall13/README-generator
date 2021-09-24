69ac1b5aeb73aeef7bfb580d6dcf4e1bba4d056e
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-09-24 14:28:17 -0400

    adding prompts for user input

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs').promises;
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const userPrompts = require('.utils/prompt.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => confirmAbout
+        }
+]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
