beafbda1e6417f3ade4ed91e4deb28a5f332b9f9
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-09-24 15:23:13 -0400

    added email validation, finished writing functions for generateMarkdown, and wrote in licensing information

--- index.js
+++ index.js
@@ -2,56 +2,116 @@
 const fs = require('fs').promises;
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
-const userPrompts = require('.utils/prompt.js')
+const emailValidator = require('email-validator');
+//const emailValidator = require('email-validator');
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt ([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
+            name: 'title',
+            message: 'What is the title of your Project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
                     return true;
                 } else {
-                    console.log('Please enter your name!');
+                    console.log('Please enter your project title!');
                     return false;
                 }
             }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an "About" section?',
-            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:',
-            when: ({ confirmAbout }) => confirmAbout
-        }
+            message: 'Please add a description of your application (required).',
+            validate: aboutInput => {
+                if (aboutInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Please provide the steps needed to install run-time dependencies.\n' + 
+            'Please format any commands with ```command```:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions for how to use the app:',
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Please write your contribution standards:'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please write your testing information:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose a license:',
+            choices: ['MIT', 'GNU', 'Apache', 'Mozilla', 'Boost', 'Unilicense', 'None']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub username (required):',
+            validate: gitHubInput => {
+                if (!gitHubInput) {
+                    console.log('You must enter your GitHub username.');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address (required):',
+            validate: userEmail => {
+                if (emailValidator.validate(userEmail)) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address!');
+                    return false;
+                }
+            },
+        },
 ]);
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+writeToFile = async (fileName, fileText) => {
+    try{
+        await fs.writeFile(fileName, fileText);
+        console.log('File written successfully in the /dist directory!');
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+init = () => {
+    console.log('Welcome to your Professional README Generator!')
+
 
 // Function call to initialize app
+
+questions()
+    .then(data => {
+        let fileText = generateMarkdown(data);
+        writeToFile('./dist/README.md', fileText);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+renderLicenseBadge = license => {
+    switch (license) {
+        case 'MIT':
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        case 'GNU':
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        case 'Apache':
+            return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        case 'Mozilla':
+            return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+        case 'Boost':
+            return '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+        case 'Unilicense':
+            return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+        default:
+            return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+renderLicenseLink = license => {
+    switch (license) {
+        case 'MIT':
+            return 'https://opensource.org/licenses/MIT';
+        case 'GNU':
+            return 'https://www.gnu.org/licenses/licenses.en.html';
+        case 'Apache':
+            return 'https://www.apache.org/licenses/LICENSE-2.0';
+        case 'Mozilla':
+            return 'https://www.mozilla.org/en-US/MPL/';
+        case 'Boost':
+            return 'https://www.boost.org/users/license.html';
+        case 'Unilicense':
+            return 'https://unlicense.org/';
+        default:
+            return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+renderLicenseSection = license => {
+    return license !== 'None' ? `This application is covered under the [${license}](${renderLicenseLink(license)}) license.` : `No licensing.`
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `# ${data.title} ${renderLicenseBadge(data.license)}
+
+<!-- screenshot here -->
+<img src="" alt="">
+## TABLE OF CONTENTS
+    1. [Description](#description)
+    2. [Installation](#installation)
+    3. [Usage](#usage)
+    4. [Contribution](#contribution)
+    5. [Testing](#testing)
+    6. [License](#license)
+    7. [Questions](#questions)
+## Description
+
+${data.about}
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+    
+${data.contribute}
+
+## Testing
+
+${data.test}
+
+## License
+
+${renderLicenseSection(data.license)}
+
+## Questions
+You can find my GitHub profile [here](https://www.github.com/${data.github}).
+Any questions please email [here] <${data.email}>!
 `;
 }
+
+
 
 module.exports = generateMarkdown;
